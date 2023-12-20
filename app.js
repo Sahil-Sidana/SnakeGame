fae7647436fa07980cf935f60710577d2c110c37
Sahil Sidana <sahilsidana2002@gmail.com>
2023-12-20 16:37:15 +0530

    onlyonedirection

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const canvas=document.getElementById('canvas');
 const pen=canvas.getContext('2d');
-pen.fillStyle='white';
+pen.fillStyle='#F0A202';
 
 
 const cellSize=67;
+const w=1200;
+const h=735;
 //top left is (0,0) 
 //(x,y,width,height)
 // pen.fillRect(100,100,190,90);
@@ -27,6 +29,21 @@ const snake={
         for(let cell of this.cells){
             pen.fillRect(cellSize*cell.x,cellSize*cell.y,cellSize-1,cellSize-1);
         }
+    },
+
+    updateSnake: function(){
+
+        const headX=this.cells[this.cells.length-1].x;
+        const headY=this.cells[this.cells.length-1].y;
+        const nextX=headX+1;
+        const nextY=headY;
+
+        this.cells.shift();
+        this.cells.push({
+            x:nextX,
+            y:nextY
+        });
+        
     }
 }
 
@@ -36,10 +53,11 @@ function init(){
 }
 
 function update(){
-    // init_x+=50;
+    snake.updateSnake();
 }
 
 function draw(){
+    pen.clearRect(0,0,w,h);
     snake.drawSnake();
 }
 
