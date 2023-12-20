1a1edc13b6f6df804cabd613c7de303aee0db4dd
Sahil Sidana <sahilsidana2002@gmail.com>
2023-12-20 17:48:06 +0530

    done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const canvas=document.getElementById('canvas');
 const pen=canvas.getContext('2d');
 
-
-
 const cellSize=67;
 const w=1200;
 const h=735;
 let food=null;
+let gameOver=false;
+let score=0;
 //top left is (0,0) 
 //(x,y,width,height)
 // pen.fillRect(100,100,190,90);
@@ -36,23 +36,59 @@ const snake={
         
         const headX=this.cells[this.cells.length-1].x;
         const headY=this.cells[this.cells.length-1].y;
-        let nextX=headX+1;
-        let nextY=headY;
+        let nextX;
+        let nextY;
+        //collison
+        if(headX===food.x && headY===food.y){
+            food=getFood();
+            score++;
+        }
+        else this.cells.shift();
 
-        if(this.direction=='left') nextX=headX-1;
+        if(this.direction=='left'){
+            nextX=headX-1;
+            nextY=headY;
+            if(cellSize*nextX<0){
+                pen.fillStyle='#FFFFFF';
+                pen.fillText('Game Over',100,100);
+                clearInterval(id);
+                gameOver=true;
+            }
+        }    
         else if(this.direction=='up'){
             nextX=headX;
             nextY=headY-1;
+            if(cellSize*nextY<0){
+                pen.fillStyle='#FFFFFF';
+                pen.fillText('Game Over',100,100);
+                clearInterval(id);
+                gameOver=true;
+            }
         }
         else if(this.direction=='down'){
             nextX=headX;
             nextY=headY+1;
+            if(cellSize*nextY>=h){
+                pen.fillStyle='#FFFFFF';
+                pen.fillText('Game Over',100,100);
+                clearInterval(id);
+                gameOver=true;
+            }
+        }
+        else{
+            nextX=headX+1;
+            nextY=headY;
+            if(cellSize*nextX>=w){
+                pen.fillStyle='#FFFFFF';
+                pen.fillText('Game Over',100,100);
+                clearInterval(id);
+                gameOver=true;
+            }
         }
         this.cells.push({
             x:nextX,
             y:nextY
         });
-        this.cells.shift();
         
     }
 }
@@ -76,20 +112,24 @@ function init(){
 }
 
 function update(){
+    if(gameOver===true){
+        clearInterval(id);
+    }
     snake.updateSnake();
 }
 
 function draw(){
     pen.clearRect(0,0,w,h);
+    pen.font='45px sans-serif'
+    pen.fillText(`Score: ${score}`,100,50);
     pen.fillStyle='#C51605'
     pen.fillRect(cellSize*food.x,cellSize*food.y,cellSize,cellSize);
     snake.drawSnake();
 }
 
 function gameLoop(){
-    console.log('game loop');
-    update();
     draw();
+    update();
 }
 
 function getFood(){
@@ -105,4 +145,4 @@ function getFood(){
 }
 
 init();
-const id = setInterval(gameLoop,200);
+const id = setInterval(gameLoop,150);
